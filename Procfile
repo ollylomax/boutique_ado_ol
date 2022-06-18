d0838dc0ef107d00c70f81c62cda2309537f023c
ollylomax <ollylomax@yahoo.com>
2022-06-18 11:54:42 +0000

    Deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boutique_ado_ol.wsgi:application
--- boutique_ado_ol/settings.py
+++ boutique_ado_ol/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@etq(-p$#)_#y7p%v=dr!4e-y)2a_2ce=gz7pvgleeeh$#q5y%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-ado-ol.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,18 @@ WSGI_APPLICATION = 'boutique_ado_ol.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
